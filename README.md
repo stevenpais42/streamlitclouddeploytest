e858b22619eb617b4e8760cd4f49cb8e3dc1e2f4
stevenpais42 <60766535+stevenpais42@users.noreply.github.com>
2023-11-08 07:13:48 -0600

    streamlit solution from data professor

---
+++ README.md
@@ -0,0 +1,10 @@
+# 🌷 Simple Prediction App
+
+A simple machine learning app that takes in input features and applies the trained model to make class prediction.
+
+The Iris dataset is used in this example app.
+
+## Demo App
+
+[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://simple-prediction.streamlitapp.com/)
+
---
+++ streamlit_app.py
@@ -0,0 +1,66 @@
+# Simple Prediction App
+# By Chanin Nantasenamat (Data Professor)
+# https://youtube.com/dataprofessor
+
+# Importing requisite libraries
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+# Page configuration
+st.set_page_config(
+     page_title='Simple Prediction App',
+     page_icon='🌷',
+     layout='wide',
+     initial_sidebar_state='expanded')
+
+# Title of the app
+st.title('🌷 Iris flower prediction app')
+
+# Load dataset
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/iris.csv')
+
+# Input widgets
+st.sidebar.subheader('Input features')
+sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.8)
+sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.1)
+petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 3.8)
+petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 1.2)
+
+# Separate to X and y
+X = df.drop('Species', axis=1)
+y = df.Species
+
+# Data splitting
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Model building
+rf = RandomForestClassifier(max_depth=2, max_features=4, n_estimators=200, random_state=42)
+rf.fit(X_train, y_train)
+
+# Apply model to make predictions
+y_pred = rf.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+
+# Print EDA
+st.subheader('Brief EDA')
+st.write('Understanding species count in the data set')
+sp_count = df.Species.value_counts(dropna=False)
+st.write(sp_count)
+st.write('Understanding species distribution in the data set')
+sp_pct = df.Species.value_counts(dropna=False, normalize=True)
+st.write(sp_pct)
+st.write('The data is grouped by the class and the variable mean is computed for each class.')
+groupby_species_mean = df.groupby('Species').mean()
+st.write(groupby_species_mean)
+st.line_chart(groupby_species_mean.T)
+
+# Print input features
+st.subheader('Input features')
+input_feature = pd.DataFrame([[sepal_length, sepal_width, petal_length, petal_width]],
+                            columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
+st.write(input_feature)
+
+# Print prediction output
+st.subheader('Output')
+st.metric('Predicted class', y_pred[0], '')
